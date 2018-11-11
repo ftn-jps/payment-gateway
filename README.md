da7e2964c7ec6fb36d86b2eff8f0b5daf1e0862b
BojanStipic <bojanstipic@gmail.com>
2018-11-11 19:43:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Payment gateway
+
+## Compiling from source
+
+```
+./mvnw package
+```
+
+## Usage
+
+```
+java -jar target/*.jar
+```
+
+Server listens on port 8081 by default. Use command line option --server.port= to change it.
---
+++ src/main/java/ftnjps/paymentgateway/PaymentGatewayApplication.java
@@ -0,0 +1,12 @@
+package ftnjps.paymentgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentGatewayApplication.class, args);
+	}
+}
