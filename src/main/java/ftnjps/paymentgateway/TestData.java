aa88f8a48e26d14357fa972e32db5ba36da94d3e
BojanStipic <bojanstipic@gmail.com>
2018-11-23 01:03:54 +0100

    Add test data

--- src/main/java/ftnjps/paymentgateway/TestData.java
+++ src/main/java/ftnjps/paymentgateway/TestData.java
@@ -2,14 +2,22 @@ package ftnjps.paymentgateway;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import ftnjps.paymentgateway.merchant.Merchant;
+import ftnjps.paymentgateway.merchant.MerchantService;
 
 @Component
 public class TestData {
 
+	@Autowired
+	private MerchantService merchantService;
+
 	@PostConstruct
 	private void init() {
-
+		Merchant m1 = new Merchant("test", "https://localhost:8085");
+		merchantService.add(m1);
 	}
 
 }
