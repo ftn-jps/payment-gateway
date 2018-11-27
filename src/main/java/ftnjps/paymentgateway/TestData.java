7fdd31d6895c6bbb5990054eb6a8a172dfde2536
BojanStipic <bojanstipic@gmail.com>
2018-11-27 21:09:17 +0100

    Update test data

--- src/main/java/ftnjps/paymentgateway/TestData.java
+++ src/main/java/ftnjps/paymentgateway/TestData.java
@@ -7,17 +7,31 @@ import org.springframework.stereotype.Component;
 
 import ftnjps.paymentgateway.merchant.Merchant;
 import ftnjps.paymentgateway.merchant.MerchantService;
+import ftnjps.paymentgateway.transaction.Transaction;
+import ftnjps.paymentgateway.transaction.TransactionService;
 
 @Component
 public class TestData {
 
 	@Autowired
 	private MerchantService merchantService;
+	@Autowired
+	private TransactionService transactionService;
 
 	@PostConstruct
 	private void init() {
 		Merchant m1 = new Merchant("test", "https://localhost:8085");
 		merchantService.add(m1);
+
+		Transaction t1 = new Transaction(5000,
+				"test",
+				"testtest",
+				14,
+				"WORKING",
+				"SOMETHING NOT WORKING",
+				"NOTHING IS WORKING");
+		t1.setToken("111");
+		transactionService.add(t1);
 	}
 
 }
