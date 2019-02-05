7286256641b7c025656ad617edee34abd200b2f5
Ivan <ivan248959@gmail.com>
2019-02-05 21:32:56 +0100

    Added 4 different bitcoin tokens(apps) for test merchants.

--- src/main/java/ftnjps/paymentgateway/TestData.java
+++ src/main/java/ftnjps/paymentgateway/TestData.java
@@ -28,28 +28,32 @@ public class TestData {
 				"1111-1111",
 				"https://localhost:8085",
 				"ATpIbDWRhS_L3c46Iz9-qgE1l_Iisfg6u4luC-JOPxfp2Klg6VrRz1ANceUgXSH-OTEckdBLwIKrs4Ug",
-				"EDDxtGUSysQafC--ODcd8jKoE8ZFNNa3tNB82I4u4lTPU3cNjeV3DsWO8NhE3H1mK46Euz0ZtOD5HNBO"
+				"EDDxtGUSysQafC--ODcd8jKoE8ZFNNa3tNB82I4u4lTPU3cNjeV3DsWO8NhE3H1mK46Euz0ZtOD5HNBO",
+				"on4xJZ6dcHsh2y15RJasFc3hxf52Kf8uxmvYTC3b"
 				);
 		merchantService.add(m1);
 		Merchant m2 = new Merchant(
 				"2222-2222",
 				"https://localhost:8085",
 				"ATpIbDWRhS_L3c46Iz9-qgE1l_Iisfg6u4luC-JOPxfp2Klg6VrRz1ANceUgXSH-OTEckdBLwIKrs4Ug",
-				"EDDxtGUSysQafC--ODcd8jKoE8ZFNNa3tNB82I4u4lTPU3cNjeV3DsWO8NhE3H1mK46Euz0ZtOD5HNBO"
+				"EDDxtGUSysQafC--ODcd8jKoE8ZFNNa3tNB82I4u4lTPU3cNjeV3DsWO8NhE3H1mK46Euz0ZtOD5HNBO",
+				"exVoz3vPTSzMR2zhp-sPVndb2xGmEmPaEH72V9ab"
 				);
 		merchantService.add(m2);
 		Merchant m3 = new Merchant(
 				"3333-3333",
 				"https://localhost:8085",
 				"ATpIbDWRhS_L3c46Iz9-qgE1l_Iisfg6u4luC-JOPxfp2Klg6VrRz1ANceUgXSH-OTEckdBLwIKrs4Ug",
-				"EDDxtGUSysQafC--ODcd8jKoE8ZFNNa3tNB82I4u4lTPU3cNjeV3DsWO8NhE3H1mK46Euz0ZtOD5HNBO"
+				"EDDxtGUSysQafC--ODcd8jKoE8ZFNNa3tNB82I4u4lTPU3cNjeV3DsWO8NhE3H1mK46Euz0ZtOD5HNBO",
+				"FzQjbFWsjfH4LtVzwse6c33hGBWa1fiYag8g24ou"
 				);
 		merchantService.add(m3);
 		Merchant m4 = new Merchant(
 				"4444-4444",
 				"https://localhost:8085",
 				"ATpIbDWRhS_L3c46Iz9-qgE1l_Iisfg6u4luC-JOPxfp2Klg6VrRz1ANceUgXSH-OTEckdBLwIKrs4Ug",
-				"EDDxtGUSysQafC--ODcd8jKoE8ZFNNa3tNB82I4u4lTPU3cNjeV3DsWO8NhE3H1mK46Euz0ZtOD5HNBO"
+				"EDDxtGUSysQafC--ODcd8jKoE8ZFNNa3tNB82I4u4lTPU3cNjeV3DsWO8NhE3H1mK46Euz0ZtOD5HNBO",
+				"oYHbxBkuBPtPu2c1FDszn7EQ1jq3YKjB1xeC-7ob"
 				);
 		merchantService.add(m4);
 
