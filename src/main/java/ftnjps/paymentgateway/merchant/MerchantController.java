b1563ba81d9d4bec8a10137243bb0fceefdf605f
Filip Petrovic <fpetrovic@ymail.com>
2019-02-06 20:32:14 +0100

    Added merchant controller with endpoint for adding new merchant

--- src/main/java/ftnjps/paymentgateway/merchant/MerchantController.java
+++ src/main/java/ftnjps/paymentgateway/merchant/MerchantController.java
@@ -1,19 +1,33 @@
 package ftnjps.paymentgateway.merchant;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/merchant")
 public class MerchantController {
 
+    @Autowired
+    private MerchantService merchantService;
+
     @PostMapping("/add")
-    public ResponseEntity<?> addMerchant(@RequestBody @Valid Merchant merchant){
-        return null;
+    public ResponseEntity<?> addMerchant(@RequestBody final Merchant merchant){
+        Merchant existing = merchantService.findByMerchantId(merchant.getMerchantId());
+        if(existing != null) {
+            return new ResponseEntity<>(
+                "Merchant with id " + merchant.getMerchantId() + " already exists",
+                HttpStatus.BAD_REQUEST
+            );
+        }
+
+        merchantService.add(merchant);
+
+        return new ResponseEntity<>(merchant,HttpStatus.CREATED);
     }
 }
